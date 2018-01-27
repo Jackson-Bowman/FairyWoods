e2d40285d496b116db5692c276259ae207d996b3
barrettand <barrett.and@husky.neu.edu>
2018-01-26 20:54:59 -0500

    CurrentOwlScript
    Owl In Progress

---
+++ FairyWoods/Assets/OwlControlScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlControlScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera.main.transform.position = transform.position - new Vector3(0, 0, 10);
+        if (Input.GetKeyDown(KeyCode.W)) {
+           GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 500));
+           //GetComponent<Rigidbody2D>().gravityScale = 0.001f;
+        }
+	}
+}
