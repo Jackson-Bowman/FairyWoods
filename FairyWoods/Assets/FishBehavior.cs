c03a8cd20f87e596b24fe3ab71b10911299c6953
Jackson Bowman <jacksoncrossbowman@gmail.com>
2018-01-27 01:32:05 -0500

    added fish behavior and cleaned up floating fail

---
+++ FairyWoods/Assets/FishBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBehavior : MonoBehaviour {
+
+	public float speed;
+	public int turnFrame;
+	private int currentFrame;
+	private Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		currentFrame = 0;
+		direction = Vector3.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (direction * speed * Time.deltaTime);
+		if (currentFrame < turnFrame) {
+			currentFrame++;
+		} else {
+			currentFrame = 0;
+			direction = -direction;
+		}
+	}
+}
--- FairyWoods/Assets/Scripts/PlayerController.cs
+++ FairyWoods/Assets/Scripts/PlayerController.cs
@@ -8,17 +8,10 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	private Animator anim;
 	public string mode;
-	private bool floating;
-	private int floatingFrames;
-	public float buoyancy;
-	public int floatFrameMax;
-	public float sink;
-	public int floatFrameSwitch;
 
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
-		bool floating = false;
-		floatingFrames = 0;
+
 		//anim = transform.GetComponentInChildren<Animator> ();
 		//anim = GetComponent<Animator>();
 		//anim.enabled = false;
@@ -29,48 +22,6 @@ public class PlayerController : MonoBehaviour {
 		if (mode == "fairy") {
 			FairyUpdate ();
 		}
-		if (floating) {
-
-//			if (floatingFrames <= floatFrameSwitch) {
-//				rb.AddForce (Vector3.up * buoyancy* Time.deltaTime);
-//			}
-//			if (floatingFrames == floatFrameMax) {
-//				floatingFrames = 0;
-//			} else {
-//				floatingFrames++;
-//			}
-
-			if (floatingFrames <= floatFrameSwitch) {
-				transform.Translate (Vector3.down * buoyancy * Time.deltaTime);
-			} else {
-				transform.Translate (Vector3.up * buoyancy * Time.deltaTime);
-			}
-			if (floatingFrames == floatFrameMax) {
-				floatingFrames = 0;
-			} else {
-				floatingFrames++;
-			}
-
-//			if (floatingFrames <= floatFrameSwitch) {
-//				rb.AddForce (new Vector3 (0, sink, 0));
-//			} else {
-//				rb.AddForce (new Vector3 (0, buoyancy, 0));
-//			}
-//			if (floatingFrames == floatFrameMax) {
-//				floatingFrames = 0;
-//			} else {
-//				floatingFrames++;
-//			}
-			
-//			if (floatingFrames == 0) {
-//				rb.AddForce (new Vector3 (0, buoyancy * Time.deltaTime, 0));
-//			}
-//			if (floatingFrames < floatFrameMax) {
-//				floatingFrames++;
-//			} else {
-//				floatingFrames = 0;
-//			}
-		}
 	}
 
 	void FairyUpdate() {
@@ -79,22 +30,5 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce (movement * speed);
 
-	}
-		
-	void OnTriggerEnter(Collider other) {
-		Debug.Log ("triggered");
-		floating = true;
-		rb.useGravity = false;
-		rb.velocity = Vector3.zero;
-		rb.isKinematic = true;
-	}
-
-	void OnTriggerExit(Collider other) {
-		floating = false;
-		floatingFrames = 0;
-		rb.useGravity = true;
-//		rb.isKinematic = true;
-//		rb.useGravity = false;
-//		rb.AddForce (new Vector3 (0, buoyancy, 0));
 	}
 }
