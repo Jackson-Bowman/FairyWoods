b799f114e30d25c1ecb23719a712f6e620a50549
Jackson Bowman <jacksoncrossbowman@gmail.com>
2018-01-28 11:10:22 -0500

    added control screen

---
+++ FairyWoods/Assets/Scripts/ModalController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModalController : MonoBehaviour {
+
+	private GameObject controlPanel;
+	void Start() {
+		controlPanel = GameObject.Find ("Control Screen");
+		controlPanel.SetActive (false);
+	}
+	public void OpenControls() {
+		controlPanel.SetActive (true);
+	}
+
+	public void CloseControls() {
+		controlPanel.SetActive (false);
+	}
+
+}
